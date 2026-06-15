400fd838c02c3b0d9aaddd7a09f03237ec700fcb
erkzzn <eriksrocha15@gmail.com>
2026-06-14 22:20:02 -0300

    Add files via upload

---
+++ aula_2/aula_2/exercicio14/exercicio14.js
@@ -0,0 +1,15 @@
+let lado1
+let lado2
+let lado3
+
+lado1 = prompt ("Informe o primeiro lado")
+lado2 = prompt ("Informe o segundo lado")
+lado3 = prompt ("Informe o terceiro lado")
+
+if( lado1 + lado2 > lado3 &&
+    lado1 + lado3 > lado2 &&
+    lado2+ lado3 > lado1
+){
+
+    
+}
---
+++ aula_2/aula_2/exercicio4/exercicio4.js
@@ -0,0 +1,12 @@
+const senha = 1234
+
+let senhaInformada
+
+senhaInformada = prompt("Informe sua senha")
+
+if (senha == senhaInformada) {
+    alert("SENHA CORRETA!")
+}
+else {
+    alert("SENHA INCORRETA!")
+}
